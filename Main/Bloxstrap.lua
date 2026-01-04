37009bfeeb66ab4b86c08c0bad8fef22330dfebc
0kenyah <abzz2024xd@gmail.com>
2026-01-04 01:45:27 -0300

    Create Bloxstrap.lua

---
+++ Main/Bloxstrap.lua
@@ -0,0 +1,9 @@
+getgenv().autosetup = {
+    path = "Bloxstrap",
+    setup = true
+}
+
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/TU_USUARIO/TU_REPO/main/Main/Initiate.lua",
+    true
+))()
